45e1e21b5affb39edf96b4a94cc5f9bc8fb15820
Keith Thibodeaux <keith@railscoder.net>
2024-08-03 19:12:50 -0400

    add homerow modifiers for shift, super, and control

--- keymap.c
+++ keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "custom/homerow_modifiers.h"
 #include "custom/tapdance.h"
 
 enum layers {
@@ -9,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_5x6(
     _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
     KC_DEL,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-    _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
+    _______, MY_A,    MY_R,    KC_S,    MY_T,    KC_D,        KC_H,    MY_N,    KC_E,    MY_I,    MY_O,    _______,
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
                    _______, _______,                                            _______, _______,
                                      _______,    MY_SPC,      KC_ENT,  _______,
