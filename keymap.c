a1e0e22d1d9612bc6f6c05d9f39b10e56b03f4f5
Keith Thibodeaux <keith@railscoder.net>
2024-08-03 18:11:07 -0400

    space tap dances to underscore

--- keymap.c
+++ keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "kthibodeaux.h"
 
 enum layers {
   _BASE
@@ -11,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
                    _______, _______,                                            _______, _______,
-                                     _______,    KC_SPC,      KC_ENT,  _______,
+                                     _______,    MY_SPC,      KC_ENT,  _______,
                                      KC_BSPC,    _______,     _______, KC_TAB,
                                      KC_ESC,     _______,     _______, _______
   )
