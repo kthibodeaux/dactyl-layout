89e0af5f013905d39a5c48ad3f00e06901dc74db
Keith Thibodeaux <keith@railscoder.net>
2024-08-04 05:48:24 -0400

    add i3 and numbers

--- keymap.c
+++ keymap.c
@@ -1,9 +1,11 @@
 #include QMK_KEYBOARD_H
 #include "custom/homerow_modifiers.h"
+#include "custom/i3.h"
 #include "custom/tapdance.h"
 
 enum layers {
-  _BASE
+  _BASE,
+  _NUM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -23,8 +25,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, MY_A,    MY_R,    KC_S,    MY_T,    KC_D,        KC_H,    MY_N,    KC_E,    MY_I,    MY_O,    _______,
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
                    _______, _______,                                            _______, _______,
-                                     _______,    MY_SPC,      KC_ENT,  _______,
+                                     MO(_NUM),   MY_SPC,      KC_ENT,  _______,
                                      KC_BSPC,    _______,     _______, KC_TAB,
                                      KC_ESC,     _______,     _______, _______
+  ),
+  [_NUM] = LAYOUT_5x6(
+    _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+    _______, I3_1,    I3_2,    I3_3,    I3_4,    I3_5,        I3_6,    I3_7,    I3_8,    I3_9,    I3_0,    _______,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, I3M_1,   I3M_2,   I3M_3,   I3M_4,   I3M_5,       I3M_6,   I3M_7,   I3M_8,   I3M_9,   I3M_0,   _______,
+                      _______, _______,                                         _______, _______,
+                                     _______,    _______,    KC_UP,   _______,
+                                     _______,    _______,    _______, KC_DOWN,
+                                     _______,    _______,    _______, _______
   )
 };
